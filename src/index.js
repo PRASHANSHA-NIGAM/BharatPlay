178fd3fcfb7a5ac3064dfc141993c6e051a99118
Prashansha Nigam <nigamprashansha@gmail.com>
2026-02-07 13:35:21 +0530

    add user registration and authentication functionality; refactor app structure

--- src/index.js
+++ src/index.js
@@ -1,43 +1,22 @@
-// import mongoose from "mongoose";
-// import { DB_NAME } from "./constants.js";
-
-// import express from "express";
-// const app = express();
-// (async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-//         app.on("error", (error) => {
-//             console.log("ERROR", error);
-//             throw error
-//         })
-//         app.listen(process.env.PORT, () => {
-//             console.log(`Server is running on port ${process.env.PORT}`)
-//         })
-//     } catch (error) {
-//         console.log("ERROR:", error)
-//         throw err
-//     }
-// })()
-
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import express from 'express';
-import connectDB from './db/index.js';
-import { DB_NAME } from './constants.js';
-
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import {app} from './app.js'
 dotenv.config({
-    path: './env'
-});
-// console.log("Mongo URL:", process.env.MONGODB_URL);
+    path: './.env'
+})
+
 
 
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
 })
 .catch((err) => {
-    console.log("Error connecting to MongoDB:", err);
+    console.log("MONGO db connection failed !!! ", err);
 })
+
+
 
